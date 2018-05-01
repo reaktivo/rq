8ee4cee866e8daa8c2e36f2dca2d1842e510dea3
Marcel Miranda <m@reaktivo.com>
2018-05-01 23:43:22 +0200

    Added empty tests that should be implemented

---
+++ src/shouldTransform.test.js
@@ -0,0 +1,6 @@
+const { test, equal } = require("tap");
+const shouldTransform = require("./shouldTransform");
+
+test("check if source code should be transformed", async () => {
+  // TODO
+});
---
+++ src/transform.test.js
@@ -0,0 +1,6 @@
+const { test, equal } = require("tap");
+const transform = require("./transform");
+
+test("transforms source code", async () => {
+  // TODO
+});
